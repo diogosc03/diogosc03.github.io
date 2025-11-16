6382b2116a1ace159c5435da78301489d89283dd
DiogoSC <diogosc03@gmail.com>
2025-11-16 13:15:40 -0300

    feat: add sections about and contact

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 const toggleTheme = document.getElementById("s-toggle-theme");
 const rootHmtl = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".s-accordion__header");
+const menuLinks = document.querySelectorAll(".s-menu__link");
 
 function changeTheme() {
   const currentTheme = rootHmtl.getAttribute("data-theme");
@@ -8,7 +10,25 @@ function changeTheme() {
   else rootHmtl.setAttribute("data-theme", "dark");
 
   toggleTheme.classList.toggle("bi-sun");
-  toggleTheme.classList.toggle("bi-moon-stars");
+  toggleTheme.classList.toggle("bi-moon");
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive
+      ? accordionItem.classList.remove("active")
+      : accordionItem.classList.add("active");
+  });
+});
+
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
