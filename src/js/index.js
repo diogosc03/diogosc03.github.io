1fa776dba96a5f3be810364cdecbdfb369c745d7
DiogoSC <diogosc03@gmail.com>
2026-01-17 03:13:13 -0300

    build: create project's structure

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const body = document.body;
+const toggleButton = document.querySelector("#toggle__icon");
+let toggled = false;
+
+function changeTheme() {
+  if (toggled) {
+    body.classList.remove("light-theme");
+    body.classList.add("dark-theme");
+    toggled = false;
+  } else {
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
+    toggled = true;
+  }
+
+  toggleButton.classList.toggle("bi-sun");
+  toggleButton.classList.toggle("bi-moon");
+}
+
+toggleButton.addEventListener("click", changeTheme);
