c23ac258b0dddd2c348f949396c8f08ded88461d
DiogoSC <diogosc03@gmail.com>
2026-01-19 01:09:44 -0300

    style: configure initial responsive layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
+const lightIcons = [
+  "./src/img/svg/html-light.svg",
+  "./src/img/svg/css-light.svg",
+  "./src/img/svg/js-light.svg"
+];
+const darkIcons = [
+  "./src/img/svg/html-dark.svg",
+  "./src/img/svg/css-dark.svg",
+  "./src/img/svg/js-dark.svg"
+];
+
 const body = document.body;
+const techImages = document.querySelectorAll("#icon__logo")
 const toggleButton = document.querySelector("#toggle__icon");
 let toggled = true;
 
@@ -6,10 +18,20 @@ function changeTheme() {
   if (toggled) {
     body.classList.remove("light-theme");
     body.classList.add("dark-theme");
+
+    techImages.forEach((img) => {
+      img.setAttribute("src", darkIcons[img.dataset.key])
+    });
+
     toggled = false;
   } else {
     body.classList.remove("dark-theme");
     body.classList.add("light-theme");
+
+    techImages.forEach((img) => {
+      img.setAttribute("src", lightIcons[img.dataset.key])
+    });
+
     toggled = true;
   }
 
